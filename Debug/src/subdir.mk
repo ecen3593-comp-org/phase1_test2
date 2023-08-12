f0eec2e55bfde4ede6c8d4b34a7f7a75e649b270
Tamara Lehman <tamara.lehman@colorado.edu>
2023-08-12 10:41:52 -0600

    adding all test files

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../src/phase2_test.s 
+
+OBJS += \
+./src/phase2_test.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: Codasip Assembler'
+	Phase2_final-ia-assembler.exe  -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
